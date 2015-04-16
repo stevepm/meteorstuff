17cb3169b3f69450404011d6209a7ab933fd82fc
Steven Magelowitz <Steven.magelowitz@gmail.com>
2015-04-15 21:01:28 -0700

    Submission of new teams

--- client/views/teams.js
+++ client/views/teams.js
@@ -1,5 +1,30 @@
 Template.teams.helpers({
-	teams: function(){
+	teams: function () {
 		return Teams.find();
+	},
+	creating: function () {
+		return Session.get('creating');
+	}
+});
+
+Template.teams.events({
+	"click .create": function (e, tpl) {
+		e.preventDefault();
+
+		Session.set('creating', true);
+	},
+	"submit form.form-create": function (e, tpl) {
+		e.preventDefault();
+
+		team = {name: tpl.$("input[name='name']").val()};
+		if (team.name.length) {
+			Teams.insert(team);
+			Session.set('creating', false);
+		}
+	},
+	"click .cancel": function(e) {
+		e.preventDefault();
+
+		Session.set('creating', false);
 	}
 });
