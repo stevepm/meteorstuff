1d0b71cca5a688149dbc86afa0c46d4a2b4fa55e
Steven Magelowitz <Steven.magelowitz@gmail.com>
2015-04-16 22:06:16 -0700

    Added games collection

---
+++ client/views/games.js
@@ -0,0 +1,3 @@
+Template.games.helpers({
+	games: Games.find()
+});
--- server/fixtures.js
+++ server/fixtures.js
@@ -9,3 +9,15 @@ if (Teams.find().count() === 0) {
 			return Teams.insert(data);
 		});
 }
+
+if (Games.find().count() === 0) {
+	var game = {
+		completed: false,
+		created_at: new Date(),
+		teams: [
+			{name: 'Barcelona', id: '1', score: 0},
+			{name: 'Manchester City', id: '2', score: 0}
+		]
+	};
+	Games.insert(game);
+}
